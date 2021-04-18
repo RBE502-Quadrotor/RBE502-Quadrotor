72a96d511d548614236316bdc1757029ef1346fc
phil-nye <philipni122@gmail.com>
2021-04-18 13:21:28 -0400

    added titles to each subplot and labeled each y axis

--- QuadrotorClass.m
+++ QuadrotorClass.m
@@ -37,33 +37,36 @@ classdef QuadrotorClass
         
         % Graph of x, xdot, alpha, and omega
         function plotResults(this, t, z)
+            yaxis_labels = {'x', '$\alpha$', '$\dot{x}$', '$\omega$'};
+            
             for i=1:4
                 ax(i) = subplot(2,2,i,'NextPlot','Add','Box','on','XGrid','on','YGrid','on',...
                             'Xlim',[t(1), t(end)],...
                             'TickLabelInterpreter','LaTeX','FontSize',14);
-                xlabel('t','Interpreter','LaTeX','FontSize',14);        
+                xlabel('t','Interpreter','LaTeX','FontSize',14);
+                ylabel(yaxis_labels(1,i), 'Interpreter', 'LaTeX', 'FontSize', 14);
             end
 
             plot(ax(1), t, z(:,1:3), 'LineWidth', 1.5);
             legend(ax(1), {'$x_1$', '$x_2$', '$x_3$'},... 
-                'Interpreter', 'LaTeX', 'FontSize', 8, 'Location', 'best');
-            title(ax(1), '${\bf x}$','Interpreter','LaTeX','FontSize',14);
-            xlabel(ax(1), 't','Interpreter','LaTeX','FontSize',14);
+                'Interpreter', 'LaTeX', 'FontSize', 9, 'Location', 'best');
+            title(ax(1), '$\bf Position / Time$','Interpreter','LaTeX','FontSize',10);
+%             xlabel(ax(1), 't','Interpreter','LaTeX','FontSize',14);
 
             plot(ax(3), t, z(:,4:6), 'LineWidth', 1.5);
             legend(ax(3), {'$\phi$', '$\theta$', '$\psi$'},...
-                'Interpreter', 'LaTeX', 'FontSize', 8, 'Location', 'best');
-            title(ax(3), '\boldmath$\alpha$','Interpreter','LaTeX','FontSize',14);
+                'Interpreter', 'LaTeX', 'FontSize', 9, 'Location', 'best');
+            title(ax(3), '$\bf Rotation / Time$','Interpreter','LaTeX','FontSize',10);
 
             plot(ax(2), t, z(:,7:9), 'LineWidth', 1.5);
             legend(ax(2), {'$\dot{x}_1$', '$\dot{x}_2$', '$\dot{x}_3$'},...
-                'Interpreter', 'LaTeX', 'FontSize', 8, 'Location', 'best');
-            title(ax(2), '$\dot{\bf x}$','Interpreter','LaTeX','FontSize',14);
+                'Interpreter', 'LaTeX', 'FontSize', 9, 'Location', 'best');
+            title(ax(2), '$\bf Velocity / Time$','Interpreter','LaTeX','FontSize',10);
 
             plot(ax(4), t, z(:,10:12), 'LineWidth', 1.5);
             legend(ax(4), {'$\omega_1$', '$\omega_2$', '$\omega_3$'},...
-                'Interpreter', 'LaTeX', 'FontSize', 8, 'Location', 'best');
-            title(ax(4), '\boldmath$\omega$','Interpreter','LaTeX','FontSize',14);
+                'Interpreter', 'LaTeX', 'FontSize', 9, 'Location', 'best');
+            title(ax(4), '$\bf Angular Velocity / Time$','Interpreter','LaTeX','FontSize',10);
         end
         
 %         function pos = silhouettePositions(z)
